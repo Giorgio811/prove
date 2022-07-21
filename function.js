389768c36c80d06cf30688198e88ef109a2097d6
Giorgio811 <giorgio.castrogiovanni@gmail.com>
2022-07-21 18:22:57 +0200

    new

---
+++ function.js
@@ -0,0 +1,12 @@
+function square(num){
+    return num*num;
+}
+let square_arrow = (num)=>{
+    return num*num;
+};
+
+let v1=square(4);
+let v2=square_arrow(8);
+
+console.log('normal '+v1);
+console.log('arrow '+v2);
